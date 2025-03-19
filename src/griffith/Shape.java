8b46f2d85462796be851270019f5c2e36ea5e54f
Serianu Andrei-Silviu <andreisilviu.serianu@student.griffith.ie>
2025-03-19 17:42:15 +0000

    Creating Class Shape

--- src/griffith/Shape.java
+++ src/griffith/Shape.java
@@ -1,5 +1,25 @@
 package griffith;
 
-public abstract class Shape {
+abstract class Shape {
+    private String name;
 
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+    
+    @Override
+    public String toString() {
+        return "Shape: " + name;
+    }
 }
